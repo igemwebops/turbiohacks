8ac09cbfc129fe3a78a9f4b8c5fd47072551ae50
Liz Tsai <liz.tsai929@gmail.com>
2025-08-30 22:51:55 +0800

    Update awards list by removing and adding categories

--- app/awards/page.tsx
+++ app/awards/page.tsx
@@ -10,10 +10,7 @@ export default function AwardsPage() {
           <li>Best Astrobiology Project</li>
           <li>Best Oncology Project</li>
           <li>Best Drug Discovery Project</li>
-          <li>Best Environment Project</li>
-          <li>Best Therapeutics Project</li>
-          <li>Best Diagnostics Project</li>
-          <li>Best Food &amp; Nutrition Project</li>
+          <li>Best Neuroscience Project</li>
           <li>Best Biomanufacturing Project</li>
         </ul>
 
@@ -23,16 +20,8 @@ export default function AwardsPage() {
           <li>Most Innovative Solution</li>
           <li>Best Impact Potential</li>
           <li>Best Technical Implementation</li>
-          <li>Best Presentation &amp; Storytelling</li>
-          <li>Community Choice Award</li>
         </ul>
-
-        <h2>Fun Awards</h2>
-        <ul>
-          <li>Spirit of the Hackathon</li>
-          <li>Midnight Hacker Award</li>
-          <li>Best Meme Award</li>
-        </ul>
+        
       </main>
     </div>
   );
